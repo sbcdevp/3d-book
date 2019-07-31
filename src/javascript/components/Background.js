1eabf66b449289206de3cf757e65e899358d2301
Sergiu_Bo <bocanceaserge335566@gmail.com>
2019-07-31 17:17:23 +0200

    add load

--- src/javascript/components/Background.js
+++ src/javascript/components/Background.js
@@ -2,20 +2,19 @@ window.experience = window.experience || {};
 window.experience.background = {
     initBackground: function () {
         'use strict';
-        this.canvas = document.querySelector('.images-background');
-        this.context = this.canvas.getContext("2d");
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
         this.imgTab = [];
         for (var i = 0; i < 3; i++) {
             this.img = new Image();
             this.img.src = "images/"+ i +".png";
-            this.setValues(i);
-            this.animate()
+            this.img.onload = this.setValues.bind(this, i);
         };
 
     },
     setValues: function (i){
+        this.canvas = document.querySelector('.images-background');
+        this.context = this.canvas.getContext("2d");
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
         this.imgProps = {
             img: this.img,
             src: this.img.src,
@@ -26,6 +25,7 @@ window.experience.background = {
             imgHeight: this.img.height / 16
         };
         this.imgTab.push(this.imgProps);
+        this.animate();
     },
     animate: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
